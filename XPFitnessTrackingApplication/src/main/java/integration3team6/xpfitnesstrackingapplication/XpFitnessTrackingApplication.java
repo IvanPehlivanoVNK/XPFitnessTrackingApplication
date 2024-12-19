fd4b1b459aed821af30f959db57de007d2e73718
Ivan Ivanov Pehlivanov <ivanivanov.pehlivanov@student.kdg.be>
2024-12-19 23:10:49 +0200

    add project

---
+++ XPFitnessTrackingApplication/src/main/java/integration3team6/xpfitnesstrackingapplication/XpFitnessTrackingApplication.java
@@ -0,0 +1,13 @@
+package integration3team6.xpfitnesstrackingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpFitnessTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpFitnessTrackingApplication.class, args);
+    }
+
+}
---
+++ XPFitnessTrackingApplication/src/test/java/integration3team6/xpfitnesstrackingapplication/XpFitnessTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package integration3team6.xpfitnesstrackingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpFitnessTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
